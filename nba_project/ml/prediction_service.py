2ccd2417aec5cc477ff4531aa5f7cba6e712f6b5
D-art9 <devan.t.court@gmail.com>
2026-06-17 16:19:49 +0530

    perf: lazy load pytorch to fix Gunicorn OOM boot crash

--- nba_project/ml/prediction_service.py
+++ nba_project/ml/prediction_service.py
@@ -1,12 +1,12 @@
-import torch
-from .model_loader import pytorch_loader
-from .feature_builder import build_team_vector, build_matchup_features
-
 def predict_matchup_win_prob(team_a, team_b):
     """
     Given two CustomTeam models, calculates their win probabilities 
     using the loaded PyTorch model and player embeddings.
     """
+    import torch
+    from .model_loader import pytorch_loader
+    from .feature_builder import build_team_vector, build_matchup_features
+
     # Get the 5 players for each team
     players_a = [tp.player for tp in team_a.team_players.all()]
     players_b = [tp.player for tp in team_b.team_players.all()]
@@ -30,3 +30,4 @@ def predict_matchup_win_prob(team_a, team_b):
         "team_a_win_probability": round(team_a_prob, 4),
         "team_b_win_probability": round(team_b_prob, 4)
     }
+
